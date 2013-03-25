4dbcb0e21df38af489c14c89265a3ea4ff3f3de6
Mattia Larentis <mattia@larentis.eu>
2013-03-25 22:24:28 +0100

    add .gitignore

--- code.c
+++ code.c
@@ -2,20 +2,42 @@
 #include <fcntl.h>
 #include <stdio.h>
 
+char * getXOR(char * s1, char *s2, int size) {
+  char * tmp = malloc(size * sizeof(char));
+  int i;
+
+  for (i = 0; i < size; i++)
+    *(tmp+i) = s1[i] ^ s2[i];
+
+  *(tmp+i) = '\0';
+  return tmp;
+}
+
 int main() {
-    char s[] = "ciao ciao";
-    char * r = malloc(sizeof(s) * sizeof(char));
-    int i;
-    char c;
-    int rfd = open("/dev/random", O_RDONLY);
-    for (i = 0; i < sizeof(s) - 1; i++) {
-        read(rfd, &c, sizeof(char));
-        printf("%d ", abs(c)); 
-        *(r+i) = abs(c) % 26 + 65;
-    }
-    *(r+i) = '\0';
-    close(rfd);
-    printf("%s\n", s);
-    printf("%s\n", r);
-    return 0;
+  char s[] = "ciao lumachina puppa, ma anche bellissima";
+  int size = sizeof s;
+  char * r = malloc(size * sizeof(char));
+  char * se;
+  char * sd;
+  int i;
+  char c;
+
+  int rfd = open("/dev/random", O_RDONLY);
+
+  for (i = 0; i < size - 1; i++) {
+    read(rfd, &c, sizeof(char));
+    *(r+i) = abs(c) % 26 + 65;
+  }
+  *(r+i) = '\0';
+  close(rfd);
+
+  se = getXOR(s, r, size);
+  sd = getXOR(r, se, size);
+
+  printf("INPUT: %s\n", s);
+  printf("R: %s\n", r);
+  printf("SE: %s\n", se);
+  printf("SD: %s\n", sd);
+
+  return 0;
 }

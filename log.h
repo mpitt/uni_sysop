e02428d507fb65d68c491f5f55d47cde6051a64d
Michele Pittoni <michele@pittoni.org>
2013-05-30 22:27:02 +0200

    Add credits to all files

--- log.h
+++ log.h
@@ -1,3 +1,14 @@
+/* Università degli Studi di Trento
+ * A.A. 2012/2013 - Corso di Laurea in Informatica
+ * Sistemi operativi 1
+ * Progetto 2: Threads
+ *
+ * Autori:
+ * Mattia Larentis 151889
+ * Andrea Panizza 151887
+ * Michele Pittoni 152549
+ */
+
 #ifndef LOG_H
 #define LOG_H
 
---
+++ raxor.h
@@ -0,0 +1,11 @@
+/* Università degli Studi di Trento
+ * A.A. 2012/2013 - Corso di Laurea in Informatica
+ * Sistemi operativi 1
+ * Progetto 2: Threads
+ *
+ * Autori:
+ * Mattia Larentis 151889
+ * Andrea Panizza 151887
+ * Michele Pittoni 152549
+ */
+
